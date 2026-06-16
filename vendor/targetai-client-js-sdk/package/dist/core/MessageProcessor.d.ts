958e447f45f049399d8114ec1f1617f830dd68ab
Andrei <bagley.aa@mail.ru>
2026-06-16 12:34:21 +0400

    Remove vendor sdk

--- vendor/targetai-client-js-sdk/package/dist/core/MessageProcessor.d.ts
+++
@@ -1,6 +0,0 @@
-import { Message } from '../types';
-export declare class MessageProcessor {
-    private lastTimestamp;
-    processMessage(rawMessage: any): Message | null;
-    reset(): void;
-}
--- vendor/targetai-client-js-sdk/package/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { TargetAIWebClient } from './TargetAIWebClient';
-export * from './types';
-export * from './utils/audioUtils';
-export { MessageProcessor } from './core/MessageProcessor';
-export { AudioManager } from './core/AudioManager';
